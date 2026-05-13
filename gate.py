89d059923d083d7f8465e4f185d794d744d1bb73
Hydrolium <0627juna@gmail.com>
2026-05-14 05:59:40 +0900

    보수(not) 추가

--- gate.py
+++ gate.py
@@ -6,7 +6,7 @@ class Operator:
         self.func = func
         self.createLabel = createLabel
 
-AND = Operator('AND', lambda x, y: x and y, lambda x, y: f"({x}·{y})")
+AND = Operator('AND', lambda x, y: x and y, lambda x, y: f"({x}{y})") # ·
 NAND = Operator('NAND', lambda x, y: not (x and y), lambda x, y: f"({x}{y})'")
 
 OR = Operator('OR', lambda x, y: x or y, lambda x, y: f"({x}+{y})")
@@ -18,6 +18,9 @@ NXOR = Operator('NXOR', lambda x, y: x == y, lambda x, y: f"({x}⊙ {y})")
 class Component:
     def __init__(self, label):
         self.label = label
+
+    def __invert__(self):
+        return ComplementExpression(self)
 
     def __mul__(self, other):
         if isinstance(other, (int, bool)):
@@ -66,6 +69,16 @@ class Component:
             other = Constant(f"{other}", int(other))
         return Expression(self, other, NAND)
 
+    def nor(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(self, other, NOR)
+    
+    def nxor(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(self, other, NXOR)
+
     def __str__(self):
         return self.label
     
@@ -81,8 +94,7 @@ class ValuedComponent(Component):
         self.value = int(value)
 
 class Variable(VariableComponent):
-    def __init__(self, label):
-        super().__init__(label)
+    pass
 
 class Constant(ValuedComponent):
     pass
@@ -92,7 +104,51 @@ class Equation(ValuedComponent):
         super().__init__(label, value)
         self.usedVariables = usedVariables
 
-class Expression(VariableComponent):
+class SubstitutableComponent(VariableComponent):
+
+    @staticmethod
+    def flat(component, values):
+        if isinstance(component, SubstitutableComponent):
+            e = component.substitute(values)
+            return e.label, e.value
+        elif isinstance(component, (Constant, Equation)):
+            return component.label, component.value
+        elif isinstance(component, Variable):
+            return component.label, values[component.label]
+
+    def substitute(self, values):
+        pass
+
+    @staticmethod
+    def getUpdatedUsedVariables(variable, initialTuple):
+        if isinstance(variable, Variable):
+            return initialTuple + (variable,)
+        elif isinstance(variable, (Expression, Equation, ComplementExpression)):
+            return initialTuple + (*variable.usedVariables,)
+        
+        return initialTuple
+
+
+class ComplementExpression(SubstitutableComponent):
+    def __init__(self, variable):
+        super().__init__(f"{variable.label}'")
+        self.variable = variable
+
+        self.usedVariables = tuple(dict.fromkeys(
+            self.getUpdatedUsedVariables(variable, tuple())
+        ))
+
+    def substitute(self, values):
+            
+        label, value = self.flat(self.variable, values)
+
+        return Equation(
+            f"{label}'",
+            int(not value),
+            self.usedVariables
+        )
+
+class Expression(SubstitutableComponent):
     def __init__(self, left, right, operator):
         super().__init__(None)
 
@@ -100,50 +156,24 @@ class Expression(VariableComponent):
         self.right = right
         self.operator = operator
 
-        self.usedVariables = []
-
-        if isinstance(left, Variable):
-            self.usedVariables.append(left)
-        elif isinstance(left, (Expression, Equation)):
-            self.usedVariables += left.usedVariables
-
-        if isinstance(right, Variable):
-            self.usedVariables.append(right)
-        elif isinstance(right, (Expression, Equation)):
-            self.usedVariables += right.usedVariables
-
-        self.usedVariables = list(dict.fromkeys(self.usedVariables))
+        self.usedVariables = tuple(dict.fromkeys(
+            self.getUpdatedUsedVariables(
+                right, self.getUpdatedUsedVariables(
+                    left, tuple()))))
 
     @property
     def label(self):
-
-        def flat(component):
-            # if isinstance(component, (Expression, Constant, Equation, Variable)):
-            return component.label
-            
-        leftLabel = flat(self.left)
-        rightLabel = flat(self.right)
-
-        return self.operator.createLabel(leftLabel, rightLabel)
+        return self.operator.createLabel(self.left.label, self.right.label)
 
     @label.setter
     def label(self, value):
-        self._label = value
+        pass
 
     # values: {$variableLabel: $variableValue, ...}
     def substitute(self, values):
 
-        def flat(component):
-            if isinstance(component, Expression):
-                e = component.substitute(values)
-                return e.label, e.value
-            elif isinstance(component, (Constant, Equation)):
-                return component.label, component.value
-            elif isinstance(component, Variable):
-                return component.label, values[component.label]
-            
-        leftLabel, leftValue = flat(self.left)
-        rightLabel, rightValue = flat(self.right)
+        leftLabel, leftValue = self.flat(self.left, values)
+        rightLabel, rightValue = self.flat(self.right, values)
 
         return Equation(
             self.operator.createLabel(leftLabel, rightLabel),
@@ -151,76 +181,168 @@ class Expression(VariableComponent):
             self.usedVariables
         )
     
+class Result:
+    def __init__(self, prod, values):
+        self.prod = prod
+        self.values = values
 
-def makeProd(variables):
+    def __str__(self):
+        return f"{tuple(self.prod.values())} {" | ".join([str(v) for v in self.values])}"
 
-    def update(variable, prod, p={}):
-        if isinstance(variable, VariableComponent):
-            prod.append({**p, variable.label: 0})
-            prod.append({**p, variable.label: 1})
-        elif isinstance(variable, ValuedComponent):
-            prod.append({**p, variable.label: variable.value})
-
-    def mp(variable, previous):
-        newProd = []
-
-        if len(previous) == 0:
-            update(variable, newProd)
-            return tuple(newProd)
-
-        for p in previous:
-            update(variable, newProd, p)
-
-        return tuple(newProd)
-
-    labels = []
-    prod = tuple()
-    for variable in variables:
-        labels.append(variable.label)
-        prod = mp(variable, prod)
-
-    return tuple(labels), prod
-
-def test(expression, onlyResult=False):
-    r = []
-
-    labels, prod = makeProd(expression.usedVariables)
-    print(f"({", ".join(labels)})", expression.label)
-    for p in prod:
-        result = expression.substitute(p)
-        print(tuple(p.values()), end=" -> ")
-        if onlyResult:
-            print(result.label, "=", result.value)
-            r.append((result.label, result.value))
-        else:
-            print(result.value)
-            r.append(result.value)
-    return r
-
-def compare(*expressions, onlyResult=False):
+class Simulator:
     
-    usedVariables = list(dict.fromkeys(sum(map(lambda x: x.usedVariables, expressions), [])))
-    labels, prod = makeProd(usedVariables)
+    @staticmethod
+    def printCell(s, width):
+        print(f"{str(s):>{width}}", end = " ")
 
-    print(f"({", ".join(labels)})", " | ".join(map(lambda expression: expression.label, expressions)))
+    @staticmethod
+    def createVariableLabel(usedVariables):
+        return f"({", ".join(v.label for v in usedVariables)})"
 
-    def createSubstitutedLabel(expression, p):
-        result = expression.substitute(p)
-        return f"{result.value}" if onlyResult else f"{result.label} = {result.value}"
+    @staticmethod
+    def do(*expressions, variableSequence=None, variableSorted=False):
+        
+        test = Simulator(*expressions, variableSequence=variableSequence, variableSorted=variableSorted)
 
-    for p in prod:
-        results = map(lambda x: createSubstitutedLabel(x, p), expressions)
-        print(tuple(p.values()), end=" ")
-        print(" | ".join(results))
+        widths = []
 
-a = Variable("a")
-b = Variable("b")
-c = Variable("c")
-d = Variable("d")
-e = Variable("e")
-f = Variable("f")
+        vl = Simulator.createVariableLabel(test.usedVariables)
+        print(vl, end=" ")
 
-v = test((a ^ b)*(c))
-print(v)
+        widths.append(len(vl))
 
-compare(a+b, b+c, onlyResult=True)
+        for label in test.labels:
+            print(label, end=" ")
+            widths.append(len(label))
+
+        print()
+
+        for result in test.testResult:
+            Simulator.printCell("(" + ", ".join(str(v) for v in result.prod.values()) + ")", widths[0])
+            for value, width in zip(result.values, widths[1:]):
+                Simulator.printCell(value, width)
+            print()
+
+        return test
+
+    @staticmethod
+    def doT(*expressions, variableSequence=None, variableSorted=False):
+        
+        test = Simulator(*expressions, variableSequence=variableSequence, variableSorted=variableSorted)
+        
+        widths = []
+
+        vl = Simulator.createVariableLabel(test.usedVariables)
+        width = max(len(lab) for lab in [Simulator.createVariableLabel(test.usedVariables), *test.labels])
+        
+        Simulator.printCell(vl, width)
+
+        widths.append(width)
+
+        for prod in test.prods:
+            cell = "(" + ", ".join(str(v) for v in prod.values()) + ")"
+            print(cell, end=" ")
+            widths.append(len(cell))
+
+        print()
+
+        for i, label in enumerate(test.labels):
+            Simulator.printCell(label, widths[0])
+
+            for result, width in zip(test.testResult, widths[1:]):
+                Simulator.printCell(result.values[i], width)
+            print()
+
+        return test
+
+    def __init__(self, *expressions, variableSequence=None, variableSorted=False):
+        self.expressions = expressions
+
+        self.labels = [expression.label for expression in expressions]
+        self.usedVariables = tuple(dict.fromkeys(
+            var for exp in expressions for var in exp.usedVariables
+        ))
+
+        if variableSequence != None:
+            if set(self.usedVariables) != set(variableSequence):
+                raise ValueError("사용된 변수들의 중복없는 튜플이 필요합니다.")
+            self.usedVariables = variableSequence
+        
+        if variableSorted:
+            self.usedVariables = tuple(sorted(self.usedVariables, key=lambda v: v.label))
+
+        self.prods = self.makeProds()
+        self.testResult = self.test()
+
+    def makeProds(self):
+        def getProdWithVariable(variable, prev):
+
+            if isinstance(variable, VariableComponent):
+                return tuple([{**prev, variable.label: val} for val in (0, 1)])
+            elif isinstance(variable, ValuedComponent):
+                return ({**prev, variable.label: variable.value}, )
+            
+            return (prev,)
+
+        prod = [{}]
+        for variable in self.usedVariables:
+            prod = [pwv for p in prod for pwv in getProdWithVariable(variable, p)]
+
+        return prod
+
+    def test(self):
+        return tuple([Result(p, tuple(exp.substitute(p).value for exp in self.expressions)) for p in self.prods])
+
+if __name__ == "__main__":
+    
+    a = Variable("a")
+    b = Variable("b")
+    c = Variable("c")
+    d = Variable("d")
+    e = Variable("e")
+    f = Variable("f")
+
+    O = Constant("1", 1)
+
+    X = a.nand(b.nand(c))
+    Y = (a.nand(b)).nand(c)
+    Simulator.doT(X, Y, variableSorted=True)
+
+    print()
+
+    A = a^b^c
+    A1 = a^(b^c)
+    B = a.nxor(b).nxor(c)
+    B1 = a.nxor(b.nxor(c))
+    C = (a^b).nxor(c)
+    C1 = a^(b.nxor(c))
+    D = (a.nxor(b))^c
+    D1 = (a.nxor(b^c))
+
+    Simulator.doT(A,A1,B,B1,C,C1,D,D1, variableSorted=True)
+
+
+"""
+A NAND (B NAND C)
+(A NAND B) NAND C
+
+A XOR B XOR C
+A NXOR B NXOR C
+A XOR B NXOR C
+A NXOR B XOR C
+
+(a, b, c) (0, 0, 0) (0, 0, 1) (0, 1, 0) (0, 1, 1) (1, 0, 0) (1, 0, 1) (1, 1, 0) (1, 1, 1) 
+(a(bc)')'         1         1         1         1         0         0         0         1 
+((ab)'c)'         1         0         1         0         1         0         1         1 
+
+  (a, b, c) (0, 0, 0) (0, 0, 1) (0, 1, 0) (0, 1, 1) (1, 0, 0) (1, 0, 1) (1, 1, 0) (1, 1, 1) 
+((a⊕ b)⊕ c)         0         1         1         0         1         0         0         1 
+(a⊕ (b⊕ c))         0         1         1         0         1         0         0         1 
+((a⊙ b)⊙ c)         0         1         1         0         1         0         0         1 
+(a⊙ (b⊙ c))         0         1         1         0         1         0         0         1 
+((a⊕ b)⊙ c)         1         0         0         1         0         1         1         0 
+(a⊕ (b⊙ c))         1         0         0         1         0         1         1         0 
+((a⊙ b)⊕ c)         1         0         0         1         0         1         1         0 
+(a⊙ (b⊕ c))         1         0         0         1         0         1         1         0 
+
+"""
