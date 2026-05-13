e2e22f8c2b37bd5d9345d510007f79e4436cb7f3
Hydrolium <0627juna@gmail.com>
2026-05-13 17:39:09 +0900

    연산자 오버로딩

--- gate.py
+++ gate.py
@@ -1,109 +1,226 @@
 import pandas as pd
 
 class Operator:
-    def __init__(self, name, func):
+    def __init__(self, name, func, createLabel):
         self.name = name
         self.func = func
+        self.createLabel = createLabel
 
-AND = Operator('AND', lambda x, y: x and y)
-NAND = Operator('NAND', lambda x, y: not (x and y))
+AND = Operator('AND', lambda x, y: x and y, lambda x, y: f"({x}·{y})")
+NAND = Operator('NAND', lambda x, y: not (x and y), lambda x, y: f"({x}{y})'")
 
-OR = Operator('OR', lambda x, y: x or y)
-NOR = Operator('NOR', lambda x, y: not (x or y))
+OR = Operator('OR', lambda x, y: x or y, lambda x, y: f"({x}+{y})")
+NOR = Operator('NOR', lambda x, y: not (x or y), lambda x, y: f"({x}+{y})'")
 
-XOR = Operator('XOR', lambda x, y: x != y)
-NXOR = Operator('NXOR', lambda x, y: x == y)
+XOR = Operator('XOR', lambda x, y: x != y, lambda x, y: f"({x}⊕ {y})")
+NXOR = Operator('NXOR', lambda x, y: x == y, lambda x, y: f"({x}⊙ {y})")
 
-class Setting:
-    def __init__(self, *operators, reversed=False):
-        self.operators = operators
-        self.reversed = reversed
+class Component:
+    def __init__(self, label):
+        self.label = label
 
-class Gate:
+    def __mul__(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(self, other, AND)
+    
+    def __rmul__(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(other, self, AND)
+    
+    def __and__(self, other):
+        return self.__mul__(other)
+    
+    def __rand__(self, other):
+        return self.__rmul__(other)
+    
+    def __add__(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(self, other, OR)
+    
+    def __radd__(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(other, self, OR)
 
-    @staticmethod
-    def makeProd(n):
-        yield from (tuple(map(int, format(i, f'0{n}b'))) for i in range(2**n))
+    def __or__(self, other):
+        return self.__add__(other)
 
-    @staticmethod
-    def operateVariable(variable, setting, out):
+    def __ror__(self, other):
+        return self.__radd__(other)
+    
+    def __xor__(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(self, other, XOR)
 
-        length = len(variable)
+    def __rxor__(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(other, self, XOR)
 
-        if length - 1 != len(setting.operators):
-            raise ValueError(f'{length -1}개의 연산자가 필요합니다.')
-        
-        varn = 'abcdefghijklmnopqrstuvwxyz'
+    def nand(self, other):
+        if isinstance(other, (int, bool)):
+            other = Constant(f"{other}", int(other))
+        return Expression(self, other, NAND)
 
-        out.setdefault('variable', []).append(variable)
+    def __str__(self):
+        return self.label
+    
+    def __repr__(self):
+        return self.label
+    
+class VariableComponent(Component):
+    pass
 
-        if setting.reversed:
+class ValuedComponent(Component):
+    def __init__(self, label, value):
+        super().__init__(label)
+        self.value = int(value)
 
-            cal = variable[-1]
+class Variable(VariableComponent):
+    def __init__(self, label):
+        super().__init__(label)
 
-            out.setdefault(varn[length -1], []).append(variable[length -1])
+class Constant(ValuedComponent):
+    pass
 
-            for i in range(length - 2, -1, -1):
-                cal = 1 if setting.operators[i].func(variable[i], cal) else 0
+class Equation(ValuedComponent):
+    def __init__(self, label, value, usedVariables):
+        super().__init__(label, value)
+        self.usedVariables = usedVariables
 
-                out.setdefault(f'{setting.operators[i].name} {varn[i]}', []).append(variable[i])
-                out.setdefault(f'R{length - i -2}', []).append(cal)
+class Expression(VariableComponent):
+    def __init__(self, left, right, operator):
+        super().__init__(None)
 
-            return cal
-        
+        self.left = left
+        self.right = right
+        self.operator = operator
 
-        cal = variable[0]
+        self.usedVariables = []
 
-        out.setdefault(varn[0], []).append(variable[0])
+        if isinstance(left, Variable):
+            self.usedVariables.append(left)
+        elif isinstance(left, (Expression, Equation)):
+            self.usedVariables += left.usedVariables
 
-        for i in range(0, length - 1):
+        if isinstance(right, Variable):
+            self.usedVariables.append(right)
+        elif isinstance(right, (Expression, Equation)):
+            self.usedVariables += right.usedVariables
 
-            cal = 1 if setting.operators[i].func(cal, variable[i + 1]) else 0
+        self.usedVariables = list(dict.fromkeys(self.usedVariables))
 
-            out.setdefault(f'{setting.operators[i].name} {varn[i + 1]}', []).append(variable[i + 1])
-            out.setdefault(f'R{i}', []).append(cal)
+    @property
+    def label(self):
 
-        return cal
+        def flat(component):
+            # if isinstance(component, (Expression, Constant, Equation, Variable)):
+            return component.label
+            
+        leftLabel = flat(self.left)
+        rightLabel = flat(self.right)
 
-    @staticmethod
-    def title(setting):
-        varn = 'abcdefghijklmnopqrstuvwxyz'
+        return self.operator.createLabel(leftLabel, rightLabel)
 
-        if setting.reversed:
-            return f"{' '.join([f'({varn[i]} {g.name}' for i, g in enumerate(setting.operators)])} {varn[len(setting.operators)]}{')' * len(setting.operators)}"
+    @label.setter
+    def label(self, value):
+        self._label = value
 
-        return f"{'(' * len(setting.operators)}{varn[0]} {' '.join([f'{g.name} {varn[i+1]})' for i, g in enumerate(setting.operators)])}"
+    # values: {$variableLabel: $variableValue, ...}
+    def substitute(self, values):
 
+        def flat(component):
+            if isinstance(component, Expression):
+                e = component.substitute(values)
+                return e.label, e.value
+            elif isinstance(component, (Constant, Equation)):
+                return component.label, component.value
+            elif isinstance(component, Variable):
+                return component.label, values[component.label]
+            
+        leftLabel, leftValue = flat(self.left)
+        rightLabel, rightValue = flat(self.right)
 
-    @staticmethod
-    def do(setting):
+        return Equation(
+            self.operator.createLabel(leftLabel, rightLabel),
+            self.operator.func(leftValue, rightValue),
+            self.usedVariables
+        )
+    
 
-        out = {}
-        for p in Gate.makeProd(len(setting.operators) + 1):
-            Gate.operateVariable(p, setting, out)
+def makeProd(variables):
 
-        return pd.DataFrame(out).set_index('variable')
+    def update(variable, prod, p={}):
+        if isinstance(variable, VariableComponent):
+            prod.append({**p, variable.label: 0})
+            prod.append({**p, variable.label: 1})
+        elif isinstance(variable, ValuedComponent):
+            prod.append({**p, variable.label: variable.value})
 
+    def mp(variable, previous):
+        newProd = []
 
-    @staticmethod
-    def compare(*settings, checkSame=False):
-        df = pd.DataFrame([Gate.do(setting).iloc[:, -1] for setting in settings], index=[Gate.title(setting) for setting in settings])
-        if checkSame:
-            df.loc['isSame'] = (df.nunique() == 1).astype(int)
-        return df
+        if len(previous) == 0:
+            update(variable, newProd)
+            return tuple(newProd)
 
-set0 = Setting(XOR, AND, reversed=False)
-print("cal::", Gate.title(set0))
-print(Gate.do(set0))
+        for p in previous:
+            update(variable, newProd, p)
 
-print()
+        return tuple(newProd)
 
-set1 = Setting(XOR, AND, reversed=True)
-print("cal::", Gate.title(set1))
-print(Gate.do(set1))
+    labels = []
+    prod = tuple()
+    for variable in variables:
+        labels.append(variable.label)
+        prod = mp(variable, prod)
 
-print(Gate.compare(Setting(NAND, NAND, reversed=True), Setting(NAND, NAND)))
+    return tuple(labels), prod
 
-print(Gate.compare(Setting(XOR, XOR), Setting(NXOR, NXOR)))
+def test(expression, onlyResult=False):
+    r = []
 
-print(Gate.compare(Setting(XOR, XOR), Setting(NXOR, XOR), checkSame=True))
+    labels, prod = makeProd(expression.usedVariables)
+    print(f"({", ".join(labels)})", expression.label)
+    for p in prod:
+        result = expression.substitute(p)
+        print(tuple(p.values()), end=" -> ")
+        if onlyResult:
+            print(result.label, "=", result.value)
+            r.append((result.label, result.value))
+        else:
+            print(result.value)
+            r.append(result.value)
+    return r
+
+def compare(*expressions, onlyResult=False):
+    
+    usedVariables = list(dict.fromkeys(sum(map(lambda x: x.usedVariables, expressions), [])))
+    labels, prod = makeProd(usedVariables)
+
+    print(f"({", ".join(labels)})", " | ".join(map(lambda expression: expression.label, expressions)))
+
+    def createSubstitutedLabel(expression, p):
+        result = expression.substitute(p)
+        return f"{result.value}" if onlyResult else f"{result.label} = {result.value}"
+
+    for p in prod:
+        results = map(lambda x: createSubstitutedLabel(x, p), expressions)
+        print(tuple(p.values()), end=" ")
+        print(" | ".join(results))
+
+a = Variable("a")
+b = Variable("b")
+c = Variable("c")
+d = Variable("d")
+e = Variable("e")
+f = Variable("f")
+
+v = test((a ^ b)*(c))
+print(v)
+
+compare(a+b, b+c, onlyResult=True)
